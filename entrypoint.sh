e17c79d164e7dad63e66439b1b132e72500ab536
andrea <mishtakuandrea@gmail.com>
2024-08-26 01:55:27 +0200

    fix: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-# Wait for 60 seconds to ensure that the database service is fully up and running
+# Wait to ensure that the database service is fully up
 sleep 180
 # Log
 echo "Application preparing to start" 
 
-# Initialize and migrate the database
-flask db init || true
-flask db migrate -m "Initial migration." || true
-flask db upgrade
+echo "Application preparing to start"
+
+# Check if the 'migrations' folder exists
+if [ ! -d "migrations" ]; then
+    # Initialize and migrate the database
+    echo "Initializing and migrating the database"
+    flask db init || true
+    flask db migrate -m "Initial migration." || true
+    flask db upgrade
+else
+    echo "Migrations folder already exists, skipping database initialization and migration"
+fi
 
 # Start the application
 exec "$@"
